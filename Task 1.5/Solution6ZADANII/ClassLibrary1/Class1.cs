1c9f2a64cd1982443d3f5a8bbeda9a4e0ddf0f6b
CHAINSAWXERI <123538769+CHAINSAWXERI@users.noreply.github.com>
2023-09-21 21:35:09 +0300

    edwtgwwer5

---
+++ Task 1.5/Solution6ZADANII/ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassLibrary1;
+
+public class Book<T>
+{
+    public T Id { get;}
+    public int PagesCount { get;}
+    public string Name { get;}
+    public string Author { get;}
+    
+    public Book(T id, string name, string author, int pagesCount)
+    {
+        Id = id; 
+        Name = name;
+        Author = author;
+        PagesCount = pagesCount;
+    }
+
+    public override string ToString()
+    {
+        return Id + " " + Name + ", " + Author + ", " + PagesCount + " Страниц";
+    }
+}
---
+++ Task 1.5/Solution6ZADANII/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary1;
+
+static void Main(string[] args)
+{
+
+
+    Book<int> Book1 = new Book<int>(1000, "book1", "Autor1", 300);
+    Book<int> Book2 = new Book<int>(2000, "book2", "Autor1", 700);
+    Book<string> Book3 = new Book<string>("uku", "book3", "Autor1", 900);
+    Book<string> Book4 = new Book<string>("kuk", "book4", "Autor1", 80);
+    Book<Guid> Book5 = new Book<Guid>(new Guid(), "book5", "Autor1", 3000);
+    Book<Guid> Book6 = new Book<Guid>(new Guid(), "book6", "Autor1", 20);
+
+
+    Book1.ToString();
+    Book2.ToString();
+    Book3.ToString();
+    Book4.ToString();
+    Book5.ToString();
+    Book6.ToString();
+}
