13038ce0c41db873cc730175afe36628ac3b9d9e
CHAINSAWXERI <123538769+CHAINSAWXERI@users.noreply.github.com>
2023-09-22 12:26:08 +0300

    ррр

---
+++ Task 1.5/Solution6ZADANII2/ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassLibrary1;
+
+public class Kucha<T>
+{
+    public T Valve { get; set; }
+
+    public Kucha(T valve)
+    {
+        this.Valve = valve;
+    }
+
+    public void Reset()
+    {
+        Valve = default(T);
+    }
+}
---
+++ Task 1.5/Solution6ZADANII2/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary1;
+
+static void Main(string[] args)
+{
+    Book<int> Book1 = new Book<int>(1000, "book1", "Autor1", 300);
+    Kucha<int> Obj1 = new Kucha<int>(300);
+    Kucha<Book<int>> Obj2 = new Kucha<Book<int>>(Book1);
+    Console.WriteLine(Obj1);
+    Console.WriteLine(Obj2);
+    Obj1.Reset();
+    Obj2.Reset();
+    Console.WriteLine(Obj1);
+    Console.WriteLine(Obj2);
+}
